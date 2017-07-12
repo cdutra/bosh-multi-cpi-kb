02a83d1c980a57a76eb114dea34580c712baad22
Chris Dutra <cdutra@pivotal.io>
2017-07-12 04:52:01 -0700

    Add delete environment scripts
    - update docs

--- README.md
+++ README.md
@@ -1,7 +1,85 @@
 # bosh-multi-cpi-kb
 
+### Deploy multi cpi on AWS
+
+```
+./deploy-multi-cpi.sh output-dir
+```
+
+To destroy the whole environment:
+```
+./deploy-multi-cpi.sh output-dir destroy
+```
+
 # TODO
 
-0. Upload heavy-stemcells
+### Upload heavy stemcells
+
+Uploading heavy stemcells fails:
+```
+bosh2 upload-stemcell bosh-stemcell-3421.9-aws-xen-hvm-ubuntu-trusty-go_agent.tgz
+Using environment '34.226.175.131' as client 'admin'
+
+###################################################### 100.00% 671.95 KB/s 9m16s
+Task 8
+
+10:40:51 | Update stemcell: Extracting stemcell archive (00:00:03)
+10:40:54 | Update stemcell: Verifying stemcell manifest (00:00:00)
+10:40:58 | Update stemcell: Checking if this stemcell already exists (cpi: multi-cpi-az1) (00:00:00)
+10:40:58 | Update stemcell: Uploading stemcell bosh-aws-xen-hvm-ubuntu-trusty-go_agent/3421.9 to the cloud (cpi: multi-cpi-az1) (00:04:23)
+10:45:21 | Update stemcell: Save stemcell bosh-aws-xen-hvm-ubuntu-trusty-go_agent/3421.9 (ami-c30408d5) (cpi: multi-cpi-az1) (00:00:00)
+10:45:26 | Update stemcell: Checking if this stemcell already exists (cpi: multi-cpi-az2) (00:00:00)
+10:45:26 | Update stemcell: Uploading stemcell bosh-aws-xen-hvm-ubuntu-trusty-go_agent/3421.9 to the cloud (cpi: multi-cpi-az2) (00:00:04)
+            L Error: CPI error 'Bosh::Clouds::CloudError' with message 'Could not locate the current VM with id 'i-08ebf9dfbd47fba1f'.Ensure that the current VM is located in the same region as configured in the manifest.' in 'create_stemcell' CPI method
+
+10:45:30 | Error: CPI error 'Bosh::Clouds::CloudError' with message 'Could not locate the current VM with id 'i-08ebf9dfbd47fba1f'.Ensure that the current VM is located in the same region as configured in the manifest.' in 'create_stemcell' CPI method
+
+Started  Wed Jul 12 10:40:51 UTC 2017
+Finished Wed Jul 12 10:45:30 UTC 2017
+Duration 00:04:39
+
+Task 8 error
+
+Uploading stemcell file:
+  Expected task '8' to succeed but state is 'error'
+
+Exit code 1
+```
+
+This happens because AWS CPI performs the following steps to create a stemcell:
+1. [Lookup for BOSH director instance](https://github.com/cloudfoundry-incubator/bosh-aws-cpi-release/blob/28c682a500f528c1bb1dac17bbf517c073397d56/src/bosh_aws_cpi/lib/cloud/aws/cloud.rb#L641) on non-default region.
+2. Create disk
+3. Attach disk to BOSH Director VM
+4. Take snapshot of attached disk
+5. Create image from snapshot
+
+### Deleting stemcells
+
+Deleting light stemcells fails:
+```
+bosh2 clean-up --all
+Using environment '34.226.175.131' as client 'admin'
+
+Continue? [yN]: y
+
+Task 12
+
+10:59:03 | Deleting stemcells: bosh-aws-xen-hvm-ubuntu-trusty-go_agent/3421.11 (00:00:09) (00:00:14)
+            L Error: Attempt to delete object did not result in a single row modification (Rows Deleted: 0, SQL: DELETE FROM "stemcells" WHERE ("id" = 4))
+
+10:59:17 | Error: Attempt to delete object did not result in a single row modification (Rows Deleted: 0, SQL: DELETE FROM "stemcells" WHERE ("id" = 4))
+
+Started  Wed Jul 12 10:59:03 UTC 2017
+Finished Wed Jul 12 10:59:17 UTC 2017
+Duration 00:00:14
+
+Task 12 error
+
+Cleaning up resources:
+  Expected task '12' to succeed but state is 'error'
+
+Exit code 1
+```
+
 0. Use https://github.com/dpb587/openvpn-bosh-release to setup vpn between vpcs in different regions
 0. Deploy 3rd cpi to gcp
--- deploy-multi-cpi.sh
+++ deploy-multi-cpi.sh
@@ -1,16 +1,4 @@
-!#/bin/env bash
-
-set -e
-
-: ${output_dir:=$1}
-: ${AWS_ACCESS_KEY:?}
-: ${AWS_SECRET_KEY:?}
-: ${AZ_1:="multi-cpi-az1"}
-: ${AZ_2:="multi-cpi-az2"}
-
-echo "Output directory set to: '/tmp/${output_dir}/'."
-
-[[ -d "/tmp/${output_dir}" ]] || mkdir "/tmp/${output_dir}"
+#!/usr/bin/env bash
 
 function generate_az_env_file {
   local AWS_ACCESS_KEY=$1
@@ -23,11 +11,11 @@ function generate_az_env_file {
   local az2_region=$6
 
   # generate ssh key pair
-  [[ -f "/tmp/${output_dir}/key" ]] || ssh-keygen -b 4096 -t rsa -f "/tmp/${output_dir}/key" -P ""
-  local public_key=$( cat "/tmp/${output_dir}/key.pub" )
+  [[ -f "${output_dir}/key" ]] || ssh-keygen -b 4096 -t rsa -f "${output_dir}/key" -P ""
+  local public_key=$( cat "${output_dir}/key.pub" )
 
 
-  cat > "/tmp/${output_dir}/${az1_env_name}.env" <<EOF
+  cat > "${output_dir}/${az1_env_name}.env" <<EOF
   export ENV_NAME=${az1_env_name}
   export AWS_ACCESS_KEY=${AWS_ACCESS_KEY}
   export AWS_SECRET_KEY=${AWS_SECRET_KEY}
@@ -35,13 +23,31 @@ function generate_az_env_file {
   export AWS_PUBLIC_KEY="${public_key}"
 EOF
 
-  cat > "/tmp/${output_dir}/${az2_env_name}.env" <<EOF
+  cat > "${output_dir}/${az2_env_name}.env" <<EOF
   export ENV_NAME=${az2_env_name}
   export AWS_ACCESS_KEY=${AWS_ACCESS_KEY}
   export AWS_SECRET_KEY=${AWS_SECRET_KEY}
   export AWS_REGION=${az2_region}
   export AWS_PUBLIC_KEY="${public_key}"
 EOF
+}
+
+function teardown_iaas {
+  local env_file=$1
+  local vpc_cidr_block=$2 # e.g. 10.0.0.0/16
+
+  source "${env_file}"
+
+  echo "Destroying environment '${ENV_NAME}' with vpc cidr '${vpc_cidr_block}'."
+  terraform destroy \
+    -var "access_key=${AWS_ACCESS_KEY}" \
+    -var "secret_key=${AWS_SECRET_KEY}" \
+    -var "region=${AWS_REGION}" \
+    -var "env_name=${ENV_NAME}" \
+    -var "public_key=${AWS_PUBLIC_KEY}" \
+    -var "vpc_cidr_block=${vpc_cidr_block}" \
+    -state="${output_dir}/${ENV_NAME}.tfstate" \
+    -force
 }
 
 function setup_iaas {
@@ -58,16 +64,16 @@ function setup_iaas {
     -var "env_name=${ENV_NAME}" \
     -var "public_key=${AWS_PUBLIC_KEY}" \
     -var "vpc_cidr_block=${vpc_cidr_block}" \
-    -state="${ENV_NAME}.tfstate"
+    -state="${output_dir}/${ENV_NAME}.tfstate"
 
-  local terraform_output_metadata="/tmp/${output_dir}/terraform-metadata-${ENV_NAME}.json"
+  local terraform_output_metadata="${output_dir}/terraform-metadata-${ENV_NAME}.json"
   jq -e --raw-output \
     '.modules[0].outputs | map_values(.value)' \
     "${ENV_NAME}.tfstate" > "${terraform_output_metadata}"
   echo "Terraform output metadata: ${terraform_output_metadata}"
 
   # Generate CPI Config ops file
-  local cpi_config_ops_file="/tmp/${output_dir}/cpi-config-ops-${ENV_NAME}.yml"
+  local cpi_config_ops_file="${output_dir}/cpi-config-ops-${ENV_NAME}.yml"
   bosh2 int \
     -l "${terraform_output_metadata}" \
     -v "access_key_id=${AWS_ACCESS_KEY}" \
@@ -77,40 +83,62 @@ function setup_iaas {
   echo "Generated CPI config ops file for '${ENV_NAME}'."
 
   # Generate Cloud Config ops file
-  local cloud_config_ops_file="/tmp/${output_dir}/cloud-config-ops-${ENV_NAME}.yml"
+  local cloud_config_ops_file="${output_dir}/cloud-config-ops-${ENV_NAME}.yml"
   bosh2 int \
     -l "${terraform_output_metadata}" \
     -v "env_name=${ENV_NAME}" \
     ops/cloud-config-ops.yml > "${cloud_config_ops_file}"
 
-  local manifest_ops_file="/tmp/${output_dir}/manifest-ops-${ENV_NAME}.yml"
+  local manifest_ops_file="${output_dir}/manifest-ops-${ENV_NAME}.yml"
   bosh2 int -v "env_name=${ENV_NAME}" ops/manifest-ops.yml > "${manifest_ops_file}"
   echo "Generated deployment manifest ops file for '${ENV_NAME}'."
+}
+
+function delete_director {
+  local env_file=$1
+  source "${env_file}"
+
+  local terraform_output_metadata="${output_dir}/terraform-metadata-${ENV_NAME}.json"
+
+  bosh2 -n delete-env \
+    -o ~/workspace/bosh-deployment/aws/cpi.yml \
+    -o ~/workspace/bosh-deployment/jumpbox-user.yml \
+    -o ~/workspace/bosh-deployment/external-ip-with-registry-not-recommended.yml \
+    -o ops/multi-cpi-director-aws-ops.yml \
+    -l "${output_dir}/key.yml" \
+    -l "${terraform_output_metadata}" \
+    -v "access_key_id=${AWS_ACCESS_KEY}" \
+    -v "secret_access_key=${AWS_SECRET_KEY}" \
+    -v director_name="multi-cpi-bosh" \
+    --vars-store "${output_dir}/creds.yml" \
+    ~/workspace/bosh-deployment/bosh.yml
+
+  echo "director-deleted" > "${output_dir}/director-deleted"
 }
 
 function deploy_director {
   local env_file=$1
   source "${env_file}"
 
-  local terraform_output_metadata="/tmp/${output_dir}/terraform-metadata-${ENV_NAME}.json"
+  local terraform_output_metadata="${output_dir}/terraform-metadata-${ENV_NAME}.json"
 
   bosh2 -n create-env \
     -o ~/workspace/bosh-deployment/aws/cpi.yml \
     -o ~/workspace/bosh-deployment/jumpbox-user.yml \
     -o ~/workspace/bosh-deployment/external-ip-with-registry-not-recommended.yml \
     -o ops/multi-cpi-director-aws-ops.yml \
-    -l "/tmp/${output_dir}/key.yml" \
+    -l "${output_dir}/key.yml" \
     -l "${terraform_output_metadata}" \
     -v "access_key_id=${AWS_ACCESS_KEY}" \
     -v "secret_access_key=${AWS_SECRET_KEY}" \
     -v director_name="multi-cpi-bosh" \
-    --vars-store "/tmp/${output_dir}/creds.yml" \
+    --vars-store "${output_dir}/creds.yml" \
     ~/workspace/bosh-deployment/bosh.yml
 
   export BOSH_ENVIRONMENT="$( jq -e --raw-output .external_ip "${terraform_output_metadata}" )"
   export BOSH_CLIENT=admin
-  export BOSH_CLIENT_SECRET=$( bosh2 int /tmp/${output_dir}/creds.yml --path /admin_password )
-  export BOSH_CA_CERT=$( bosh2 int /tmp/${output_dir}/creds.yml --path /director_ssl/ca )
+  export BOSH_CLIENT_SECRET=$( bosh2 int ${output_dir}/creds.yml --path /admin_password )
+  export BOSH_CA_CERT=$( bosh2 int ${output_dir}/creds.yml --path /director_ssl/ca )
 }
 
 function deployment {
@@ -120,14 +148,14 @@ function deployment {
   local release_path=$4
 
   source "${az1_env_file}"
-  local az1_ops="/tmp/${output_dir}/cpi-config-ops-${ENV_NAME}.yml"
-  local az1_cloud_config="/tmp/${output_dir}/cloud-config-ops-${ENV_NAME}.yml"
-  local az1_manifest_ops="/tmp/${output_dir}/manifest-ops-${ENV_NAME}.yml"
+  local az1_ops="${output_dir}/cpi-config-ops-${ENV_NAME}.yml"
+  local az1_cloud_config="${output_dir}/cloud-config-ops-${ENV_NAME}.yml"
+  local az1_manifest_ops="${output_dir}/manifest-ops-${ENV_NAME}.yml"
 
   source "${az2_env_file}"
-  local az2_ops="/tmp/${output_dir}/cpi-config-ops-${ENV_NAME}.yml"
-  local az2_cloud_config="/tmp/${output_dir}/cloud-config-ops-${ENV_NAME}.yml"
-  local az2_manifest_ops="/tmp/${output_dir}/manifest-ops-${ENV_NAME}.yml"
+  local az2_ops="${output_dir}/cpi-config-ops-${ENV_NAME}.yml"
+  local az2_cloud_config="${output_dir}/cloud-config-ops-${ENV_NAME}.yml"
+  local az2_manifest_ops="${output_dir}/manifest-ops-${ENV_NAME}.yml"
 
   bosh2 -n update-cpi-config <( bosh2 int -o "${az1_ops}" -o "${az2_ops}" templates/cpi-config.yml )
 
@@ -140,12 +168,49 @@ function deployment {
   bosh2 -n upload-stemcell "${stemcell_path}"
 
   pushd ~/workspace/bosh-cpi-certification/shared/assets/certification-release
-    bosh2 -n create-release --name certification --tarball /tmp/${output_dir}/certification-release.tgz --force
+    bosh2 -n create-release --name certification --tarball ${output_dir}/certification-release.tgz --force
   popd
-  bosh2 -n upload-release /tmp/${output_dir}/certification-release.tgz
+  bosh2 -n upload-release ${output_dir}/certification-release.tgz
 
   bosh2 deploy -n -d multi-cpi -o "${az1_manifest_ops}" -o "${az2_manifest_ops}" templates/certification-manifest.yml
 }
+
+set -e
+
+: ${output_dir:=$1}
+destroy=$2
+: ${AWS_ACCESS_KEY:?}
+: ${AWS_SECRET_KEY:?}
+: ${AZ_1:="multi-cpi-az1"}
+: ${AZ_2:="multi-cpi-az2"}
+
+if [[ -n "${destroy}" ]]; then
+  [[ -d "${output_dir}" ]] || echo "Environment does NOT exist!" && exit 0
+
+  if [[ ! -f "${output_dir}/director-deleted" ]]; then
+    source "${output_dir}/${AZ_1}.env"
+    terraform_output_metadata="${output_dir}/terraform-metadata-${ENV_NAME}.json"
+
+    export BOSH_ENVIRONMENT="$( jq -e --raw-output .external_ip "${terraform_output_metadata}" )"
+    export BOSH_CLIENT=admin
+    export BOSH_CLIENT_SECRET=$( bosh2 int ${output_dir}/creds.yml --path /admin_password )
+    export BOSH_CA_CERT=$( bosh2 int ${output_dir}/creds.yml --path /director_ssl/ca )
+
+    bosh2 -n -d multi-cpi delete-deployment
+    bosh2 -n clean-up --all
+
+    delete_director "${output_dir}/${AZ_1}.env"
+  fi
+
+  teardown_iaas "${output_dir}/${AZ_1}.env" "10.0.0.0/16"
+  teardown_iaas "${output_dir}/${AZ_2}.env" "10.1.0.0/16"
+
+  rm -rf ${output_dir}
+  exit 0
+fi
+
+echo "Output directory set to: '${output_dir}/'."
+[[ -d "${output_dir}" ]] || mkdir "${output_dir}"
 
 # generate az env files
 generate_az_env_file \
@@ -155,16 +220,16 @@ generate_az_env_file \
   "${AZ_2}" us-west-1
 
 # create multi cpi AZ 1 (contains bosh director)
-setup_iaas "/tmp/${output_dir}/${AZ_1}.env" "10.0.0.0/16"
+setup_iaas "${output_dir}/${AZ_1}.env" "10.0.0.0/16"
 
 # create multi cpi AZ 2
-setup_iaas "/tmp/${output_dir}/${AZ_2}.env" "10.1.0.0/16"
+setup_iaas "${output_dir}/${AZ_2}.env" "10.1.0.0/16"
 
 # deploy director to AZ 1
-deploy_director "/tmp/${output_dir}/${AZ_1}.env"
+deploy_director "${output_dir}/${AZ_1}.env"
 
 # deploy dummy deployment
 deployment \
-  "/tmp/${output_dir}/${AZ_1}.env" \
-  "/tmp/${output_dir}/${AZ_2}.env" \
+  "${output_dir}/${AZ_1}.env" \
+  "${output_dir}/${AZ_2}.env" \
   "https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-3421.11-aws-xen-hvm-ubuntu-trusty-go_agent.tgz"
